7462dc827c797588e3bc58bc715b6435e3b52547
Baybars ÖNAL <bay66bars@gmail.com>
2020-11-27 13:46:47 +0300

    ALKIS DEVAM

--- HOME/alkis/alkis.ino
+++ HOME/alkis/alkis.ino
@@ -2,13 +2,15 @@
  int ssgrs = 0;
  int lkon = 0;
  int alks1 = 0;
- int say2 = 0;
- int ses1 = 1;
- int ses2 = 1;
- int ses3 = 1;
- int say = 0;
- int fark = 0;
+ int sayi[4] = {1,3,5,7};
+ int ses[4] = {1,3,5,7};
+ int fark[2] = {1,3};
 
+
+ 
+
+
+ 
 void setup() {
   
    pinMode(ssgrs,INPUT);
@@ -21,62 +23,168 @@ void setup() {
 
 void loop()
 {  
+  
+  
+  ornek(1);
+  delay(60);
+  ornek(2);
 
-  do {
-    say = say + 1;   
- 
-} while (say2 < 2);
+
 
 
   
-   say = say+1 ;
-    if (say==1)   ses1 = analogRead(ssgrs);
-    if (say==2)  { ses2 = analogRead(ssgrs);
-     say=1;
-    }
-
-     fark = ses2-ses1;
-   if ((fark >= 40 ) && (fark <=70 ))  {  
-      //pinMode(ssgrs,OUTPUT);
-     delay(500);
-       //pinMode(ssgrs,INPUT);
-    //  ses3 = analogRead(ssgrs);
-     // fark = ses3-ses2;
-     Serial.println(fark);
-  dene();
-      
+  /*
+    say1 = 0;
    
-    
+   do {
+        say1 = say1 + 1;   
+        ses1 = analogRead(ssgrs);
+       // delay (10);
+         ses2 = analogRead(ssgrs);
+        delay (30);
+         ses3 = analogRead(ssgrs);
+      //  delay (10);
+         ses4  = analogRead(ssgrs);
+      //  delay (10);
+               
+       } while (say1 < 4);
 
-   // delay(130);
+ fark1 = ses1 - ses2;
+ if (fark1 < 0) fark1 = fark1 * (-1);
+ fark2=ses3 - ses4 ;
+if (fark2 < 0) fark2 = fark2 * (-1);
+
+if (fark1 > 1) { 
+    Serial.println("Fark1 : ");
+     Serial.println(fark1);
+}
+
+if (fark2 > 1) { 
+    Serial.println("Fark2 : ");
+     Serial.println(fark2);
+}
+ */
+ //Serial.println(ses3);
+ 
+ //Serial.println(ses4);
+ 
+ 
+    //delay(100);
+    /*
+    
+    say = say + 1;   
+   
+    if (say==1)   ses1 = analogRead(ssgrs);
+    
+    if (say==2)  
+       { ses2 = analogRead(ssgrs);
+          say=0;
+        }
+
+     fark 1 = ses2-ses1;
+
+     say3 = 0;
+   
+   do {
+        say3 = say3 + 1;   
+ 
+       } while (say3 < 15);
+
+ 
+      
     }
+    if ((fark >= 40 ) && (fark <=70 ))
+       {        
+         delay(500);
+         Serial.println(fark);
+         dene();
+       }
 
 
-    
- }
-
-
-
-
-void dene()
-{
-   say2 = 0;
-do {
-    say2 = say2 + 1;   
- 
-} while (say2 < 15);
-
-   Serial.println(say2);
-  //if (alks2== 0) alks2=1;
-  if (alks1== 1) {
-    isik(); 
-    return;
-  } 
-  if (alks1== 0) alks1=1; 
- 
+*/
 }
 
 
+void ornek(int j)
+{
+
+
+    int i = j;
+ do {
+
+
+        
+        ses[i] = analogRead(ssgrs);
+       // Serial.println(i);
+        delay (90);
+        i = i + 1;  
+        
+         ses[i] = analogRead(ssgrs);
+      //    Serial.println(i);
+       delay (90);
+      
+               
+       } while (i < j+1);
+
+  fark[i-1] = ses[i] - ses[i-1];
+  if ( fark[i-1] < 0 ) fark[i-1] = fark[i-1] * (-1);
+  
+
+  
+*/
+//Serial.println(fark[i-1]);
+if (fark[i-1] > 1) { 
+int a= i-1;
+
+
+    Serial.println(a);
+     Serial.println(fark[i-1]);
+     delay (100);
+}
+//Serial.println(ses[2]);
+//delay (100);
+  
+}
+
+
+
+void yeniden()
+{
+      for(int a= 0;a < 15; a++)
+{
+
+}
+
+if ((fark[1] > 1) && (fark[2] == 0) fark[1]=0;
+
+
+}
+
+
+
+
+
+/*
+void dene()
+{
+   say3 = 0;
+   
+   do {
+        say3 = say3 + 1;   
+ 
+       } while (say3 < 15);
+
+   if (alks1== 1) 
+      {
+        isik(); 
+        return;
+        } 
+ 
+   if (alks1== 0) alks1=1; 
+ 
+}
+
+*/
 
 void isik()
  {
@@ -93,25 +201,6 @@ void isik()
 
   alks1=0;
   
-    Serial.println(alks1); 
- 
-  delay(100);
-   
+     
  }
- /*
-  Serial.println(tit);
-  Serial.println(alks1); 
-  Serial.println(alks2);
-
-  if(alks1==1) alks2=alks2+1;
-  if (alks2==15) {alks1=0; alks2=0;}
- 
- // if (tit < 750 || tit  > 760  )  alks();
- // if (tit ==755 ) return;
-  //if ( tit==754 )  return;
-  //if ( tit==753 )  return;
-  //if ( tit==756 )  return;
-  //delay(2000);
-
-*/
  
