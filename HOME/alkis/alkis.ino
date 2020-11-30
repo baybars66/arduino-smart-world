599c31514bcf66d81f8eed53699f3e65fd54119e
Baybars ÖNAL <bay66bars@gmail.com>
2020-11-30 17:30:40 +0300

    alkis

--- HOME/alkis/alkis.ino
+++ HOME/alkis/alkis.ino
@@ -2,13 +2,10 @@
  int ssgrs = 0;
  int lkon = 0;
  int alks1 = 0;
+ int alks2 = 0;
  int sayi[4] = {1,3,5,7};
  int ses[4] = {1,3,5,7};
  int fark[2] = {1,3};
-
-
- 
-
 
  
 void setup() {
@@ -23,171 +20,86 @@ void setup() {
 
 void loop()
 {  
-  
-  
-  ornek(1);
-  delay(60);
-  ornek(2);
+   ilkornek();
+   if (alks1==0) return;
+   delay(10);
+   sonornek();
+  /* if (fark[2]>10){ isik();
+       fark[2]=0;
 
-
-
-
-  
-  /*
-    say1 = 0;
-   
-   do {
-        say1 = say1 + 1;   
-        ses1 = analogRead(ssgrs);
-       // delay (10);
-         ses2 = analogRead(ssgrs);
-        delay (30);
-         ses3 = analogRead(ssgrs);
-      //  delay (10);
-         ses4  = analogRead(ssgrs);
-      //  delay (10);
-               
-       } while (say1 < 4);
-
- fark1 = ses1 - ses2;
- if (fark1 < 0) fark1 = fark1 * (-1);
- fark2=ses3 - ses4 ;
-if (fark2 < 0) fark2 = fark2 * (-1);
-
-if (fark1 > 1) { 
-    Serial.println("Fark1 : ");
-     Serial.println(fark1);
-}
-
-if (fark2 > 1) { 
-    Serial.println("Fark2 : ");
-     Serial.println(fark2);
-}
+ }
  */
- //Serial.println(ses3);
- 
- //Serial.println(ses4);
- 
- 
-    //delay(100);
-    /*
-    
-    say = say + 1;   
-   
-    if (say==1)   ses1 = analogRead(ssgrs);
-    
-    if (say==2)  
-       { ses2 = analogRead(ssgrs);
-          say=0;
-        }
-
-     fark 1 = ses2-ses1;
-
-     say3 = 0;
-   
-   do {
-        say3 = say3 + 1;   
- 
-       } while (say3 < 15);
+  if ((alks1==1) && (alks2 == 1)) isik();
 
  
-      
-    }
-    if ((fark >= 40 ) && (fark <=70 ))
-       {        
-         delay(500);
-         Serial.println(fark);
-         dene();
-       }
-
-
-*/
 }
 
-
-void ornek(int j)
+void ilkornek()
 {
-
-
-    int i = j;
- do {
-
-
-        
-        ses[i] = analogRead(ssgrs);
-       // Serial.println(i);
-        delay (90);
-        i = i + 1;  
-        
-         ses[i] = analogRead(ssgrs);
-      //    Serial.println(i);
-       delay (90);
+   ses[1] = analogRead(ssgrs);
+// Serial.println(i);
+   delay (90);
+    
+   ses[2] = analogRead(ssgrs);
+ //Serial.println(i);
+    delay (90);
       
-               
-       } while (i < j+1);
-
-  fark[i-1] = ses[i] - ses[i-1];
-  if ( fark[i-1] < 0 ) fark[i-1] = fark[i-1] * (-1);
+  fark[1] = ses[1] - ses[2];
+  if ( fark[1] < 0 ) fark[1] = fark[1] * (-1);
+  if (fark[1]>10) {alks1=1;
   
+    Serial.println("alkıs1= 1");
+    delay(500);
+ //  Serial.println(fark[1]);
+  }
+   //if ((alks1==1) && (alks2==1)) isik();
 
+}
+void sonornek()
+{
+   ses[3] = analogRead(ssgrs);
+// Serial.println(i);
+   delay (90);
+    
+   ses[4] = analogRead(ssgrs);
+ //Serial.println(i);
+    delay (90);
+      
+  fark[2] = ses[3] - ses[4];
+  if ( fark[2] < 0 ) fark[2] = fark[2] * (-1);
+
+  if (fark[2]>3) { alks2=1;
+Serial.println("alkıs2= 1");
+  }
+  else { alks1=0;
+   alks2=0;
+   Serial.println("alkıs1 2 = 0 0 ");
+  }
   
-*/
-//Serial.println(fark[i-1]);
-if (fark[i-1] > 1) { 
-int a= i-1;
-
-
-    Serial.println(a);
-     Serial.println(fark[i-1]);
-     delay (100);
+ //  if ((fark[2] == 0) ||(fark[2]==1)) {
+   
+  //  Serial.print("f= ");
+  // Serial.println(fark[1]);
+ //   Serial.print("oldu");
+  Serial.print("f1= ");
+   Serial.println(fark[1]);
+   
+   Serial.print("f2= ");
+   Serial.println(fark[2]);
+     
+}
+/*
+if (fark[2]>10){ isik();
+   fark[2]=0;
 }
 //Serial.println(ses[2]);
 //delay (100);
   
 }
 
-
-
-void yeniden()
-{
-      for(int a= 0;a < 15; a++)
-{
-
-}
-
-if ((fark[1] > 1) && (fark[2] == 0) fark[1]=0;
-
-
-}
-
-
-
-
-
-/*
-void dene()
-{
-   say3 = 0;
-   
-   do {
-        say3 = say3 + 1;   
- 
-       } while (say3 < 15);
-
-   if (alks1== 1) 
-      {
-        isik(); 
-        return;
-        } 
- 
-   if (alks1== 0) alks1=1; 
- 
-}
-
 */
-
 void isik()
- {
+ {    Serial.println("ISIK");
    if (lkon==0)  
     { 
         digitalWrite(LED_BUILTIN, HIGH); 
@@ -200,7 +112,12 @@ void isik()
       }
 
   alks1=0;
-  
+   alks2=0;
      
  }
+
+
+
+
+
  
