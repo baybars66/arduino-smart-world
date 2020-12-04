ff7d30c7c657a14eef5c1ced7f1ab4d65c20eb27
Baybars ÖNAL <bay66bars@gmail.com>
2020-12-04 21:31:16 +0300

    sıcaklık ilavesi mikrofona

--- HOME/alkis/alkis.ino
+++ HOME/alkis/alkis.ino
@@ -1,6 +1,9 @@
 
- ****************************/
 
+#include <dht11.h> // dht11 kütüphanesini ekliyoruz.
+#define DHT11PIN 2 // DHT11PIN olarak Dijital 2"yi belirliyoruz.
+
+dht11 DHT11;
 
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
@@ -31,7 +34,7 @@ void setup() {
 }
 
 void loop()
-{  
+{  // isi();
    ilkornek();
    if (alks1==0) return;
    delay(10);
@@ -94,7 +97,7 @@ void isik()
    if (lkon==0)  
     { 
         digitalWrite(LED_BUILTIN, HIGH); 
-        rdgonder ("THBDL1ON");
+       // rdgonder ("THBDL1ON");
 
         
         lkon=1;
@@ -102,7 +105,7 @@ void isik()
   else  
      {
         digitalWrite(LED_BUILTIN, LOW); 
-         rdgonder ("THBDL1OF");
+        // rdgonder ("THBDL1OF");
         lkon=0;
       }
 
@@ -110,6 +113,39 @@ void isik()
   alks2=0;
      
  }
+
+
+
+void isi()
+{
+  // Serial.println();
+  // Sensörün okunup okunmadığını konrol ediyoruz. 
+  // chk 0 ise sorunsuz okunuyor demektir. Sorun yaşarsanız
+  // chk değerini serial monitörde yazdırıp kontrol edebilirsiniz.
+ // int chk = DHT11.read(DHT11PIN);
+//  Serial.print("CHK: ");
+ // Serial.println(chk);
+  // Sensörden gelen verileri serial monitörde yazdırıyoruz.
+//  Serial.print("Nem (%): ");
+//  Serial.println((float)DHT11.humidity, 2);
+
+  Serial.print("Sicaklik (Celcius): ");
+  Serial.println((float)DHT11.temperature, 2);
+/*
+  Serial.print("Sicaklik (Fahrenheit): ");
+  Serial.println(DHT11.fahrenheit(), 2);
+
+  Serial.print("Sicaklik (Kelvin): ");
+  Serial.println(DHT11.kelvin(), 2);
+
+  // Çiğ Oluşma Noktası, Dew Point
+  Serial.print("Cig Olusma Noktasi: ");
+  Serial.println(DHT11.dewPoint(), 2);
+
+  // 2 saniye bekliyoruz. 2 saniyede bir veriler ekrana yazdırılacak.
+  delay(2000);
+  */
+}
 
 
 
