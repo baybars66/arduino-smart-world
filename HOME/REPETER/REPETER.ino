cfa54c1b57ff5a52200e8e5c3e49457d92e69842
Baybars ÖNAL <bay66bars@gmail.com>
2020-12-10 20:17:34 +0300

    REPETER

--- HOME/REPETER/REPETER.ino
+++ HOME/REPETER/REPETER.ino
@@ -1,26 +1,32 @@
 
-/************ HOME *************
- *  NODMCU SERVERDEN GELEN BİLGİLERİ RF İLE TÜM CLİNTLARA
- *  VE RF İLE TÜM CLİENTLARDAN GELEN DATALARI MODMCUYA İLETİR
- *  ARDUİNOæç
- ****************************/
+/************ HOME REPETER*************
+ *  ÖN BAHÇEYE ULAŞMAYAN RF SINYALINI ALIP TEKRAR YOLLAMA
+ *  AYRICA L1 LAMBASINI MANUEL YAKMA VE REPORT SORULDUĞUNDA
+ *  ORTAM SICAKLIGINI İLETME
+  ****************************/
 
-
+#include <dht11.h> // dht11 kütüphanesini ekliyoruz.
 #include <RH_ASK.h>
-//#include <SPI.h> // Not actually used but needed to compile
+
+#define DHT11PIN 2 // DHT11PIN olarak Dijital 2"yi belirliyoruz.
+#define Buton 5
+
+dht11 DHT11;
 
 RH_ASK driver;
 
-//char *controller;
-
-
 String rdgelen="1";
 String rdgiden ="2";
-String dene ="2";
+String sicaklik ="";
+String gitti = "";
+
 
 
 void setup() {
- 
+  
+  pinMode(Buton, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(115200);
   if (!driver.init()) Serial.println("FHRFOFAA");
   Serial.println("hazır");
@@ -29,13 +35,32 @@ void setup() {
 
 void loop()
 {
-
+  
+ rdgelen="1";
+ 
  rfal(); //from rf
-   
+
+ if (digitalRead(Buton) == 1) isik();
+  
 }
 
 
+void isik()
+{
+  if (digitalRead(LED_BUILTIN) == LOW )
+     {
+       digitalWrite(LED_BUILTIN, HIGH);
+       rdgonder("THGNL1ON");
+      }
+  else
+     {
+       digitalWrite(LED_BUILTIN, LOW);
+       rdgonder("THGNL1OF");
+      }
+    
+    delay(500);
 
+}
 
 
   
@@ -44,35 +69,35 @@ void rfal()
     uint8_t buf[8];
     uint8_t buflen = sizeof(buf);
     if (driver.recv(buf, &buflen)) // Non-blocking
-    { 
-  
+    {  
       int i;
-       String rdgelen = (char*)buf;
-       
-       rdgelen= rdgelen.substring(0,9);
+      rdgelen = (char*)buf;
+      rdgelen= rdgelen.substring(0,8);
 
-       Serial.println(rdgelen);  
-       delay(10);
-       Serial.println(rdgelen.substring(8,9));
-       
-       if(rdgelen.substring(8,9) == "⸮") 
-       {  Serial.println("bu"); 
-        rdgelen= rdgelen.substring(0,8);
-        rdgelen=rdgelen +"R" ;
-        rdgonder(rdgelen);
+      if (rdgelen.substring(0,2) != "TH") return;
+      
+       //Serial.println(rdgelen);  
+      if (rdgelen == "THREPORT")
+         { 
+           isi();
+           return;
+         }
+
+   if (rdgelen == "THALMONN")
+       { 
+         secure();
+         return;
        }
-    
-     if(rdgelen.substring(8,9) == "R") 
-       { Serial.println("bu2"); 
-        rdgelen= "1";
+   
+   if ( rdgelen!= gitti) 
+        {
+         rdgonder(rdgelen);
+         gitti = rdgelen;
+         }
+       
+     }
 
-       }
-
-        
-      // Serial.println(dene);  
-     
-    }
-
+ 
 }
 
 
@@ -86,4 +111,33 @@ void rdgonder(String rdgiden) //
   Serial.println(msg);
   delay(10);
  }
- 
+
+
+ void isi()
+{
+
+  int chk = DHT11.read(DHT11PIN);
+  int bb =DHT11.temperature;
+  String baba = String(bb);
+  sicaklik = "FWTEMP" + baba;
+  rdgonder(sicaklik);
+
+}
+
+ void secure()
+{
+  
+ digitalWrite(LED_BUILTIN, LOW);
+ rdgonder("THGNL1OF");
+ delay (10);
+ rdgonder("THGNL2OF");
+ delay (10);
+ rdgonder("THBHSKOF");
+ delay (10);
+ rdgonder("THLRNLOF");
+ delay (10);
+ rdgonder("THBHCSOF");
+ delay (10);
+
+
+}
