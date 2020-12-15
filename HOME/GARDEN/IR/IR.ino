0c1f14a49e32dc2d2c6c2cf9b857f09066c253e1
Baybars ÖNAL <bay66bars@gmail.com>
2020-12-15 14:50:17 +0300

    nnn

--- HOME/GARDEN/IR/IR.ino
+++ HOME/GARDEN/IR/IR.ino
@@ -10,8 +10,7 @@ int sw =2;
 
 IRrecv irrecv(al);
 decode_results result;
-int deger=0X0000;
-
+int deger=1;
 
 void setup() {
   // put your setup code here, to run once:
@@ -35,75 +34,91 @@ void loop() {
 
   if(irrecv.decode(&result)) {
     deger = result.value;
-      Serial.println(deger, HEX);
+    if( deger<0) deger = deger * (-1);
+     //Serial.println(deger, HEX);
+      Serial.println(deger);
    // if( deger<0) deger = deger* (-1);
     
-  //  Serial.println(deger);
+    
      irrecv.resume();
      
   }
 
-is();
+   is();
   
 }
+
 void is()
 {
- switch (result.value) { // compare the value to the following cases
-      case 0xFFA25D: // 1 Tuşu 1 nolu ışık aç
+ switch (deger) { // compare the value to the following cases
+      case 24481: // 1 Tuşu 1 nolu ışık aç
         rdgonder ("THGNL1ON");
         break;
-      case 0xFF6897: // * Tuşu 1 Nolu Işık kapa
+      case 24735: // * Tuşu 1 Nolu Işık kapa
         rdgonder ("THGNL1OF");
         break;
-     case 0xFFE21D: //3 Tuşu 2 Nolu Işık aç
+     case 8161: //3 Tuşu 2 Nolu Işık aç
         rdgonder ("THGNL2ON");
         break;
-     case 0xFFB04F: // # tuşu 2 nolu ışık kapa
+     case 4335: // # tuşu 2 nolu ışık kapa
      rdgonder ("THGNL2OF");
          break;
-    case 0xFF629D: // 2 tuşu abajur aç
+    //case 0xFF629D: // 2 tuşu abajur aç
+      case 16575:
         digitalWrite(LED_BUILTIN, HIGH);
         digitalWrite(sw, HIGH);
         break;
-     case 0xFF9867: // 0 tuşu abajur kapa
+     case 28561: // 0 tuşu abajur kapa
         digitalWrite(LED_BUILTIN, LOW);
         digitalWrite(sw, LOW);
         break;
-     case 0xFF18E7: // Y ok tuşu Kırmız aş
+     case 16321: // Y ok tuşu Kırmız aş
         rdgonder ("THBHSKON");
         break;
-     case 0xFF4AB5: // A Ok tuşu Kırmız kapa
+     case 20655: // A Ok tuşu Kırmız kapa
        rdgonder ("THBHSKOF");
         break;
+  
+     case 8415: // A Ok tuşu Kırmız kapa
+       digitalWrite(LED_BUILTIN, LOW);
+        digitalWrite(sw, LOW);
+      rdgonder ("THGNL1OF");
+       r          dgonder ("THGNL2OF");
+       rdgonder ("THBHSKOF");
+  
+      rdgonder ("THBHCSOF");
 
-     case 0xFF10EF: // Sola tuşu Çeşme Aç
+       
+        break;
+     case 23461: // Sola tuşu Çeşme Aç
        rdgonder ("THBHCSON");
         break;
 
-     case 0xFF5AA5: // Sağa Tuşu Çeşme kapa
+     case 9435: // Sağa Tuşu Çeşme kapa
        rdgonder ("THBHCSOF");
         break;
-     case 0xFF22DD: // 4 Tuşu ..... Açma
+        
+     case 10455: // 4 Tuşu ..... Açma
        rdgonder ("THGNS4ON");
         break;
-     case 0xFFE01F: // 7 Tuşu ....... Kapama
+     case 6121: // 7 Tuşu ....... Kapama
         rdgonder ("THGNS4OF");
         break;
-     case 0xFF02FD: // 5 Tuşu ....... Açma
+     case 22441: // 5 Tuşu ....... Açma
        rdgonder ("THGNS5ON");
         break;
 
-     case 0xFFA857: // 8 Tuşu ....... Kapama
+     case 6375: // 8 Tuşu ....... Kapama
        rdgonder ("THGNS5OF");
         break;
 
-     case 0xFFC23D: // 6 Tuşu ...... Açma
+     case 26775: // 6 Tuşu ...... Açma
         rdgonder ("THGNS6ON");
         break;
-     case 0xFF906F: // 9 Tuşu ...... Kapama
+     case 26521: // 9 Tuşu ...... Kapama
        rdgonder ("THGNS6OF");
         break;
-     case 0xFF38C7: // OK TUŞU ..... AÇMA/KAPAMA
+     case 2295: // 0 TUŞU ..... AÇMA/KAPAMA
        rdgonder ("THGNOKON");
         break;
  
