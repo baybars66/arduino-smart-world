8cc3eace86fe87e7b8736939de347001be1dab2c
Baybars ÖNAL <bay66bars@gmail.com>
2021-03-16 11:30:00 +0300

    mmm

--- HOME/REPETER/REPETER.ino
+++ HOME/REPETER/REPETER.ino
@@ -5,26 +5,41 @@
  *  ORTAM SICAKLIGINI İLETME
   ****************************/
 
-#include <dht11.h> // dht11 kütüphanesini ekliyoruz.
+
+/************ HOME REPETER*************
+ * Bu program repeter olduğu için aldığı data datayı yollarken resetliyordu. Bu nedenle
+ * sent ve Rec kontrollerle alırken ve gönerirken birini açıp diğerini kapattım
+ * Bu yapıyı bozma!!!!!!!!!!!!!!!!!!!!!
+  ****************************/
+
+
+  
 #include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+#include <dht11.h> // dht11 kütüphanesini ekliyoruz.
+
 
 #define DHT11PIN 2 // DHT11PIN olarak Dijital 2"yi belirliyoruz.
-#define Buton 5
-
+#define Kapa 5
+#define Ac 9
 dht11 DHT11;
 
 RH_ASK driver;
 
 String rdgelen="1";
-String rdgiden ="2";
+//String rdgiden ="2";
 String sicaklik ="";
 String gitti = "";
 int control = 1;
-
+int sentKont=2; //****önemli
+int recKont=1;  //****önemli
 
 void setup() {
   
-  pinMode(Buton, INPUT);
+  pinMode(Ac, INPUT);
+   pinMode(Kapa, INPUT);
+  
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(115200);
@@ -34,49 +49,42 @@ void setup() {
 }
 
 void loop()
-{
+{ 
+ if (recKont==1) 
+  { rfal();
   
- if (digitalRead(Buton) == 1)
- { control = 2;
-  Serial.print("Control: ");
-  Serial.println(control);
-  isik();
- // delay(1000); /// Değiştirme
-  delay(100);
-
-   //return;
- } 
-  else 
-     {       
-       control =1;
-      // rdgelen="1";
-      rfal(); //from rf
-      }
+  }
+   delay(30);
+ dugmeKont();
+ delay(30);
+ gonderici();
+ delay(30);
  
-}
-
-
-void isik()
-{
-  if (digitalRead(LED_BUILTIN) == LOW )
-     {
-       digitalWrite(LED_BUILTIN, HIGH);
-      rdgonder("THGNL1ON");
-      //rdgonder("TWOFL1ON");
-       
-      }
-  else
-     {
-       digitalWrite(LED_BUILTIN, LOW);
-        rdgonder("THGNL1OF");
-       // rdgonder("TWOFL1OF");
-       
-      }
-    
-    
 
 }
 
+ void dugmeKont(){
+  
+ if (digitalRead(Ac))
+  {  recKont =2;
+     delay(10);
+    digitalWrite(LED_BUILTIN, HIGH);
+   // rdgonder("THGNL1ON");
+     rdgonder("TWOFL1ON");
+     delay(100);
+  }
+  
+ if (digitalRead(Kapa))
+   { recKont =2;
+     delay(10);
+     digitalWrite(LED_BUILTIN, LOW);
+   // rdgonder("THGNL1OF");
+     rdgonder("TWOFL1OF");
+      delay(100);
+   }
+
+  
+ }
 
   
 void rfal()
@@ -84,58 +92,89 @@ void rfal()
     uint8_t buf[8];
     uint8_t buflen = sizeof(buf);
     if (driver.recv(buf, &buflen)) // Non-blocking
-    { // Serial.println("Gelen var");
-      int i;
-      rdgelen = (char*)buf;
-      rdgelen= rdgelen.substring(0,8);
+      { // Serial.println("Gelen var");
+         // int i;
+         rdgelen = (char*)buf;
+         rdgelen= rdgelen.substring(0,8);
 
-      if (rdgelen.substring(0,2) != "TH") return;
-      //  Serial.print("RD GRLRN: "); 
-      Serial.println(rdgelen);  
-      if (rdgelen == "THREPORT")
+         if (rdgelen.substring(0,2) != "TW")
+          {
+            sentKont=2;
+            return;
+         }
+         else
+          {
+            Serial.print("RD GRLRN: "); 
+            Serial.println(rdgelen); 
+            sentKont= 1;
+           }
+
+      delay(1000);
+  
+      }
+    
+ }
+
+void gonderici()
+{
+
+  if( sentKont == 1) {
+    recKont =2;
+     delay(10);
+     /*
+ if (rdgelen == "TWREPORT")
          { 
            isi();
            return;
          }
 
-   if (rdgelen == "THALMONN")
+ 
+   if (rdgelen == "TWALMONN")
        { 
          secure();
          return;
        }
-
-   if ( rdgelen!= gitti) 
+*/
+  if ( rdgelen!= gitti) 
         {
+           gitti = rdgelen;
+         
          rdgonder(rdgelen);
-         gitti = rdgelen;
+        }
         
-         }
-       
-     }
-    
+
+
+         
+  sentKont=2;
+   //delay(1000);
+  }
+
+
   
-   
-
- 
 }
-
 
 
 void rdgonder(String rdgiden) //
 {
-  char *msg = "FHNNNNNN";
-  rdgiden.toCharArray(msg,9);
+     recKont =2;
+     delay(100);
+     
+  char a[]="bbtt";
+  rdgiden.toCharArray(a,9);
+  char *msg = a;
   driver.send((uint8_t *)msg, strlen(msg));
   driver.waitPacketSent();
-//  Serial.print("Gidecek");
-  //Serial.println(msg);
-//  driver.waitPacketSent();
- // delay(2000);      /// Değiştirme
-  delay(100);
-    driver.send((uint8_t *)msg, strlen(msg)); //DİKKAT 2. KEZ YOLLUYORUM
-  driver.waitPacketSent();
-    //Serial.print("Gitti: ");
-        // Serial.println(msg);
+  Serial.println(msg);
+  delay(700);
+  /*
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    Serial.println(msg);
+    delay(1000);
+    */
+
+   recKont =1;
+   delay(10);
  }
 
 
